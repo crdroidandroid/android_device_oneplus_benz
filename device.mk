cc15a9ce78f8fd22e2476bc15302738375880db0
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    benz: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -267,6 +267,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResTarget \
+    DeviceAsWebcamResTarget \
     FrameworksResTarget \
     NcmTetheringOverlay \
     OPlusFrameworksResTarget \
